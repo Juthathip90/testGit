f1aaf0b56f7f0c3a549cae49fb50af3dc662b591
Juthathip <juthathip@bettersoft.co.th>
2021-09-14 16:13:51 +0700

    Add name in trx today and sale report

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # testGit
 
 Step
-1. add Master
-
+1. add Master + Project
+2. add Develop
+3. add Feature/Pentest
+4. add Feature/Drop1
--- testGit/testGit/ViewController.swift
+++ testGit/testGit/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Return to Fix launch screen
         // Return to fix bugs PVT : Hotfixs 1 2 3 4
         // Return to fix bugs P'pom : iPhone6 stuck on launch screen
+        // Drop1 : add name in trx today and sale report
     }
 
 
